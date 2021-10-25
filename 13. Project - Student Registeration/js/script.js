e1462e4efbb6ea15fd8626eca54d4db099d2336e
santhanakrishnanstark <santhanakrishnanstark@gmail.com>
2021-10-25 22:07:13 +0530

    js project student form

--- 13. Project - Student Registeration/js/script.js
+++ 13. Project - Student Registeration/js/script.js
@@ -33,7 +33,10 @@ function handleFormData(event) {
     // store student data to global Student array list
     studentLists.push(studentObj);
     
-    console.log('Final Student List', studentLists)
+    console.log('Final Student List added...', studentLists)
+
+    // add the student data to local storage
+    updateLocalStorage(studentLists);
 
     // reset student form
     studentForm.reset();
@@ -42,8 +45,8 @@ function handleFormData(event) {
 function getStudentObject() {
 
     // create unique student ID
-    let id = studentLists.length+1;
-
+    let id = studentLists[studentLists.length - 1]?.id ? studentLists[studentLists.length - 1].id + 1 : studentLists.length+1;
+    // console.log(studentLists[studentLists.length - 1], studentLists[studentLists.length - 1]?.id + 1)
     let firstName = document.querySelector('#fname').value;
     let lastName = document.querySelector('#lname').value;
     let dateOfBirth = document.querySelector('#dob').value;
@@ -117,6 +120,12 @@ function generateTableRow(studentObj) {
 
     let deleteBtn = generateDeleteButton(studentObj.id);
 
+    // bind Event to Delete Button
+    deleteBtn.addEventListener('click', function (event) {
+        const sID = +event.target.getAttribute('action-id');
+        deleteStudentData(sID);
+    })
+
     // append action buttons
     actionViewTd.appendChild(viewBtn);
     actionDeleteTd.appendChild(deleteBtn);
@@ -127,6 +136,16 @@ function generateTableRow(studentObj) {
 
     // append student row
     sTable.querySelector('tbody').appendChild(tr);
+}
+
+function rebuidTableRows(studentLists) {
+    // remove old table data
+    const sTable = document.querySelector('.student-table');
+    sTable.querySelector('tbody').innerHTML = '';
+
+    studentLists.forEach((studentObj) => {
+        generateTableRow(studentObj);
+    })
 }
 
 function generateViewButton(id) {
@@ -160,3 +179,35 @@ function buildModalData(id) {
     
     document.querySelector('.student-popup tbody').innerHTML = popupRow.join('');
 }
+
+function deleteStudentData(id) {
+    const filteredStudentList = studentLists.filter((obj) => obj.id !== id);
+    console.log(filteredStudentList)
+    
+    // replace the StudentLists with the filteredStudentList
+    studentLists = filteredStudentList;
+
+    rebuidTableRows(studentLists);
+
+    // update LocalStorage
+    updateLocalStorage(studentLists);
+}
+
+function updateLocalStorage(studentLists) {
+    const data = JSON.stringify(studentLists);
+    localStorage.setItem('student-list', data);
+}
+
+function init() {
+    // check if any data in localStorage
+    if(localStorage.getItem('student-list').length) {
+        // convert JSON String to original data format
+        const data = JSON.parse(localStorage.getItem('student-list'));
+        studentLists = data;
+
+        rebuidTableRows(studentLists);
+
+    }
+}
+
+init();
